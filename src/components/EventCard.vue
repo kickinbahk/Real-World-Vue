e23aeed572aa333d75cae3dc4c293ff9ad753078
Josiah Mory <kickinbahk@gmail.com>
2020-04-02 18:55:34 -0600

    add prettier settings and EventCard component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 v-if=""></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
