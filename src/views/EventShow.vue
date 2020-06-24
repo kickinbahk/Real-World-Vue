662644137795522eab1ea887993f7694ebad4377
kickinbahk <kickinbahk@gmail.com>
2020-06-23 18:59:18 -0600

    create call to get event data in event show

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>Showing Event #{{ id }}</h1>
+  <h1>{{ event.title }}</h1>
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(`There was an error: ${error}`)
+      })
+  }
 }
 </script>
 
