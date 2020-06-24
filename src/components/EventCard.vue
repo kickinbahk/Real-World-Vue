34c6a8b5afd3a79720ff76ac6e0802779f7b91ea
kickinbahk <kickinbahk@gmail.com>
2020-06-23 18:07:12 -0600

    change icons to work with new way feather library works

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,13 +6,21 @@
     <div class="event-card -shadow">
       <span class="eybrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <div class="icon-wrapper">
+        <users-icon size="1.5x" class="icon"></users-icon>
+      </div>
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
 </template>
 
 <script>
+import { UsersIcon } from 'vue-feather-icons'
+
 export default {
+  components: {
+    UsersIcon
+  },
   data() {
     return {
       event: {
@@ -49,5 +57,21 @@ export default {
   color: black;
   text-decoration: none;
   font-weight: 100;
+}
+
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
 }
 </style>
